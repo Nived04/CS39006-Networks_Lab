2ed206730cc3c5e1ca5ff1b9a1b9f49081a677d4
Nived04 <shahnived04@gmail.com>
2025-02-04 15:31:38 +0530

    LA3: TCP Communication

--- Lab3/doencfileserver.c
+++ Lab3/doencfileserver.c
@@ -1,3 +1,11 @@
+/*
+Assignment 3 Submission
+Name: Nived Roshan Shah
+Roll number: 22CS10049
+Link of the pcap file: https://drive.google.com/drive/folders/1kcV_YUMqJitUH8T9vJsclaRTXmOlWNxz?usp=sharing
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,9 +15,10 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#define PORT 5050
 #define MAX_BUFF 100
+#define CHUNK_SIZE 15
 
+// function to use the key map to encrypt the plain text into cipher text 
 void encrypt(char* file_name, char* enc_file_name, char* key, int total_size) {
     FILE *fp = fopen(file_name, "r");
     FILE *fenc = fopen(enc_file_name, "w");
@@ -17,14 +26,15 @@ void encrypt(char* file_name, char* enc_file_name, char* key, int total_size) {
     char* file_content = (char*)malloc(1000*sizeof(char));
     char c, temp; 
     int i=0;
-    while(i<total_size) {
+    
+    for(int i=0; i<total_size; i++) {
         c = fgetc(fp);
+
         if(c >= 'a' && c <= 'z') {
             temp = key[c - 'a'];
             if(temp >= 'A' && temp <= 'Z') {
                 temp = temp - 'A' + 'a';
             }
-        
         }
         else if(c >= 'A' && c <= 'Z') {
             temp = key[c - 'A'];
@@ -32,48 +42,33 @@ void encrypt(char* file_name, char* enc_file_name, char* key, int total_size) {
                 temp = temp - 'a' + 'A';
             } 
         }
-        else {
-            temp = c;
-        }
+        else temp = c;
+
         c = temp;
         fputc(c, fenc);
-        i++;
     }
 
     fclose(fp);
     fclose(fenc);
 }
 
-void send_message(int client_sockfd, char* message) {
-    int message_size = strlen(message);
-    while(message_size > MAX_BUFF) {
-        send(client_sockfd, message, MAX_BUFF, 0);
-        message_size -= MAX_BUFF;
-        message += MAX_BUFF;
-    }
-    if(message_size > 0) {
-        send(client_sockfd, message, message_size, 0);
-    }
-    else {
-        send(client_sockfd, "%%", 2, 0);
-    }
-}
+// function to get the contents of the file in a string and send it in chunks
+void send_in_chunks(int* comm_sockfd, FILE* fp) {
+    char c; int i=0;
+    char* file_content = (char*)malloc(CHUNK_SIZE*sizeof(char));
 
-void receive_message(char arr[], int comm_sockfd) {
-    int flag = 0;
-    char temp[MAX_BUFF];
-    while(1) {
-        recv(comm_sockfd, temp, MAX_BUFF, 0);
-        for(int i=0; i<MAX_BUFF; i++) {
-            if(temp[i] == '%') {
-                flag = 1;
-                break;
-            }
+    while((c=fgetc(fp)) != EOF) {
+        file_content[i] = c;
+        i++;
+        if(i == CHUNK_SIZE) {
+            send(*comm_sockfd, file_content, CHUNK_SIZE, 0);
+            i = 0;
         }
-        strcat(arr, temp);
-        if(flag) break;
     }
-}
+
+    file_content[i] = '\0';
+    send(*comm_sockfd, file_content, i+1, 0);
+}   
 
 int main() {
     int server_sockfd, comm_sockfd;
@@ -91,7 +86,7 @@ int main() {
 
     server_addr.sin_family      = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port        = htons(20000);
+    server_addr.sin_port        = htons(5050);
 
     if(bind(server_sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         perror("Unable to bind local address\n");
@@ -101,7 +96,6 @@ int main() {
     listen(server_sockfd, 5);
 
     while(1) {
-
         clilen = sizeof(client_addr);
         comm_sockfd = accept(server_sockfd, (struct sockaddr *)&client_addr, &clilen);
 
@@ -109,79 +103,98 @@ int main() {
             perror("Accept error\n");
             exit(0);
         }
-        printf("Connection accepted\n");
-        
-        pid_t pid = fork();
-        if(pid == 0) {
-            close(server_sockfd);
 
+        int conn_closed = 0;
+
+        // fork to handle multiple clients
+        pid_t pid = fork();
+        if(pid == 0) { // child process
+            close(server_sockfd);
+            
             // Get client IP and port
             char client_ip[INET_ADDRSTRLEN];
             inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, INET_ADDRSTRLEN);
             int client_port = ntohs(client_addr.sin_port);
-            
+
+            printf("\nConnection accepted with client at port: %d\n\n", client_port);
+
             char plain_file_name[100], enc_file_name[105];
             sprintf(plain_file_name, "%s.%d.txt", client_ip, client_port);
             sprintf(enc_file_name, "%s.enc", plain_file_name);
 
+            // size of the file to be received
             int total_size = 0;
 
-            while(1) {
-                FILE* fp = fopen(plain_file_name, "w");
-                int flag = 0;
-                int i=0;
+            while(1){
+
                 while(1) {
-                    int num_received = recv(comm_sockfd, buf, MAX_BUFF, 0);
-                    printf("** %s \n", buf);
+                    FILE* fp = fopen(plain_file_name, "w");
+                    int flag = 0, i=0; 
+                    key_received = 0;
 
-                    int j=0;
+                    // receive until the null character is received (means end of file)
+                    while(1) {
+                        int num_received = recv(comm_sockfd, buf, MAX_BUFF, 0);
+                        int j=0;
+                        
+                        // first 26 characters that will be received, make the key
+                        if(!key_received) {
+                            while(j<num_received && (buf[j] != '\0')) {
+                                key[i] = buf[j];
+                                i++; j++;
+                            }
 
-                    if(!key_received) {
-                        while(j<num_received && (buf[j] != '\0')) {
-                            key[i] = buf[j];
-                            i++; j++;
+                            if(buf[j] == '\0') {
+                                key_received = 1;
+                                key[i] = '\0';
+                                j++;
+                                printf("Received Substitution Cipher key: %s\n", key);
+                            }
+                            else continue;
                         }
 
-                        if(buf[j] == '\0') {
-                            key_received = 1;
-                            key[i] = '\0';
+                        // if the key is received, then the rest of the message is the file content
+                        num_received-=j; // -j becaseu some part of key may be with the file.
+                        total_size += num_received;
+
+                        // printf("\n%d\n", num_received);
+                        
+                        while(num_received > 0) {
+                            fflush(NULL);
+                            fputc(buf[j], fp);
                             j++;
+                            num_received--;
+                            if(buf[j] == '\0') {
+                                flag = 1;
+                                break;
+                            }
                         }
-                        else {
-                            continue;
-                        }
+                        
+                        if(flag) break;
                     }
 
-                    printf("%s\n", key);
-                    num_received-=j;
-
-                    total_size += num_received;
-
-                    printf("\n%d\n", num_received);
-
-                    while(num_received > 0 && buf[j] != '\0') {
-                        fputc(buf[j], fp);
-                        j++;
-                        num_received--;
-                    }
-
-                    if(buf[j] == '\0') {
-                        flag = 1;
-                        break;
-                    }
+                    fclose(fp);
+                    if(flag) break;
                 }
 
-                fclose(fp);
-                if(flag) break;
+                encrypt(plain_file_name, enc_file_name, key, total_size-1);
+                printf("File encryption done\n");
+            
+                FILE* fenc = fopen(enc_file_name, "r");
+                send_in_chunks(&comm_sockfd, fenc);
+                fclose(fenc);
+
+                int x = recv(comm_sockfd, buf, 1, 0);
+                if(strcmp(buf, "N") == 0) {
+                    shutdown(comm_sockfd, SHUT_RDWR);
+                    close(comm_sockfd);
+                    exit(0);
+                }   
+
             }
-
-            printf("\n%d\n", total_size);
-
-            encrypt(plain_file_name, enc_file_name, key, total_size);
-
-            printf("FILE ENCRYPTED\n");
-            break;
         }
-        
+        else {
+            close(comm_sockfd);
+        }
     }
 }
--- Lab3/retrieveencfileclient.c
+++ Lab3/retrieveencfileclient.c
@@ -1,3 +1,10 @@
+/*
+Assignment 3 Submission
+Name: Nived Roshan Shah
+Roll number: 22CS10049
+Link of the pcap file: https://drive.google.com/drive/folders/1kcV_YUMqJitUH8T9vJsclaRTXmOlWNxz?usp=sharing
+
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,43 +14,49 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#define PORT 5050
 #define BUFFER 100
+#define CHUNK_SIZE 20
 
+// function to convert a string to uppercase
 char* to_upper(char* str) {
-    char* temp = (char*)malloc(26*sizeof(char));
-    for(int i=0; i<26; i++) {
-        if(str[i] >= 'a' && str[i] <= 'z') {
-            temp[i] = str[i] - 'a' + 'A';
-        }
-        else {
-            temp[i] = str[i];
-        }
-    }
+    int n = strlen(str);
+    char* temp = (char*)malloc(n*sizeof(char));
+
+    for(int i=0; i<n; i++) 
+         temp[i] = (str[i] >= 'a' && str[i] <= 'z') ? str[i] - 'a' + 'A' : str[i];
+
     return temp;
 }
 
-void send_message(int client_sockfd, char* message) {
+void send_key(int client_sockfd, char* message) {
     int message_size = strlen(message) + 1;
-    while(message_size > BUFFER) {
-        send(client_sockfd, message, BUFFER, 0);
-        message_size -= BUFFER;
-        message += BUFFER;
+
+    while(message_size > CHUNK_SIZE) {
+        send(client_sockfd, message, CHUNK_SIZE, 0);
+        message_size -= CHUNK_SIZE;
+        message += CHUNK_SIZE;
     }
+
     if(message_size > 0) {
         send(client_sockfd, message, message_size, 0);
     }
 }
 
-char* get_file_contents(FILE* fp) {
-    char* file_content = (char*)malloc(1000*sizeof(char));
+void send_in_chunks(int* comm_sockfd, FILE* fp) {
     char c; int i=0;
+    char* file_content = (char*)malloc((CHUNK_SIZE)*sizeof(char));
+
     while((c=fgetc(fp)) != EOF) {
         file_content[i] = c;
         i++;
+        if(i == CHUNK_SIZE) {
+            send(*comm_sockfd, file_content, CHUNK_SIZE, 0);
+            i = 0;
+        }
     }
+
     file_content[i] = '\0';
-    return file_content;
+    send(*comm_sockfd, file_content, i+1, 0);
 }
 
 int main() {
@@ -57,7 +70,7 @@ int main() {
     }
 
     server_addr.sin_family  = AF_INET;
-    server_addr.sin_port    = htons(20000);
+    server_addr.sin_port    = htons(5050);
     inet_aton("127.0.0.1", &server_addr.sin_addr);
 
     if ((connect(client_sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr))) < 0) {
@@ -65,82 +78,119 @@ int main() {
 		exit(0);
 	}
     
+    printf("Connected to server\n");
+
     char *file_name         = (char*)malloc(100*sizeof(char));
     char *not_found_error   = (char*)malloc(100*sizeof(char));
     FILE* fp;
 
+
     while(1) {
-        bzero(file_name, 100);
-        printf("Enter the file name to encrypt: ");
-        scanf("%s", file_name);
+        // Get the file name (until a valid file is entered)
+        while(1) {
+            bzero(file_name, 100);
+            printf("Enter the file name to encrypt: ");
+            scanf(" %s", file_name);
 
-        // pre-defining not-found error message to identify when received from server
-        sprintf(not_found_error, "NOTFOUND %s", file_name);
+            // pre-defining not-found error message to identify when received from server
+            sprintf(not_found_error, "NOTFOUND %s", file_name);
 
-        fp = fopen(file_name, "r");
-        if(fp == NULL) {
-            printf("Error: %s\nPlease Re-try.\n\n", not_found_error);
-        }
-        else break;
-    }
-
-    char key[27];
-    int map[26];
-    fflush(NULL);
-    while(1) {
-        for(int i=0; i<26; i++) map[i] = 0;
-        printf("Enter the key: ");
-        scanf(" %s", key);
-
-        char* temp = (char*)malloc(26*sizeof(char));
-        temp = to_upper(key);
-
-        fflush(NULL);
-
-        if(strlen(temp) == 26) {
-            int flag = 1;
-            for(int i=0; i<26; i++) {
-                map[temp[i] - 'A']++;
-                if(map[temp[i] - 'A'] > 1) {
-                    printf("Key cannot have repeated characters\n");
-                    flag = 0;
-                    break;
-                }
+            fp = fopen(file_name, "r");
+            if(fp == NULL) {
+                printf("Error: %s\nPlease Re-try.\n", not_found_error);
             }
+            else break;
+        }
+
+        // array to store the null terminated key
+        char key[27];
+        int map[26];
+        fflush(NULL); 
+
+        // Get the key (until a valid key is entered)
+        while(1) {
+            // map stores the frequency of each letter in the key
+            // required because key cannot have repeated characters
+            for(int i=0; i<26; i++) map[i] = 0;
+
+            printf("Enter the key: ");
+            scanf(" %s", key);
+
+            char* temp = (char*)malloc(26*sizeof(char));
+            temp = to_upper(key);
+
+            fflush(NULL);
+
+            if(strlen(temp) == 26) {
+                int flag = 1;
+                for(int i=0; i<26; i++) {
+                    map[temp[i] - 'A']++;
+                    // if a character is repeated, key is invalid
+                    if(map[temp[i] - 'A'] > 1) {
+                        printf("Key cannot have repeated characters\n");
+                        flag = 0;
+                        break;
+                    }
+                }
+                if(flag) break;
+            }
+            else {
+                printf("Key must be of length 26\n");
+            }
+        }
+
+        // store the contents of the file in a string
+
+
+        // send the key and file content to the server
+        send_key(client_sockfd, key);
+        send_in_chunks(&client_sockfd, fp);
+        
+        fclose(fp);
+
+        char buff[BUFFER];
+
+        char* enc_file_name = (char*)malloc(100*sizeof(char));
+        sprintf(enc_file_name, "%s.enc", file_name);
+
+        FILE* enc_file = fopen(enc_file_name, "w");
+
+        // receive until the null character is received (means end of file)
+        while(1) {
+            int num_rec = recv(client_sockfd, buff, BUFFER, 0);
+
+            // printf("--%d\n", num_rec);
+            int flag = 0;
+            for(int i=0; i<num_rec; i++) {
+                // if null char received, set flag to 1 and break
+                if(buff[i] == '\0') {
+                    flag = 1;
+                    break;
+                } 
+                fputc(buff[i], enc_file);
+            }
+            // if flag is 1, eof reached, stop receiving
             if(flag) break;
         }
+
+        fclose(enc_file);
+
+        printf("Plaintext file: %s\nCipertext file: %s\n", file_name, enc_file_name);
+
+        char choice[4];
+        printf("Do you want to encrypt another file? (Yes/No): ");
+        scanf(" %s", choice);
+
+        char msg;
+        if(strcmp(choice, "No") == 0) {
+            msg = 'N';
+            send(client_sockfd, &msg, 1, 0);
+            close(client_sockfd);
+            break;
+        }
         else {
-            printf("Key must be of length 26\n");
+            msg = 'Y';
+            send(client_sockfd, &msg, 1, 0);
         }
     }
-
-    char* file_content = get_file_contents(fp);
-
-    fclose(fp);
-
-    printf("%s\n", file_content);
-
-    send_message(client_sockfd, key);
-    send_message(client_sockfd, file_content);
-
-
-    // char c ='-';
-    // char buff[BUFFER];
-
-    // FILE* enc_file = fopen("enc_file.txt", "w");
-
-    // while(1) {
-    //     recv(client_sockfd, buff, BUFFER, 0);
-    //     int flag = 0;
-    //     for(int i=0; i<strlen(buff); i++) {
-    //         c = buff[i];
-    //         if(c == '%') {
-    //             flag = 1;
-    //             break;   
-    //         }
-    //         fprintf(enc_file, "%c", c);
-    //         bzero(buff, BUFFER);
-    //     }
-    //     if(flag == 1) break;
-    // }
 }
